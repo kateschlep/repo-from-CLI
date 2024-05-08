19fcee8cd46a95dbf0955a0c5ba5f670ce5e1c04
Kaitlyn Schleper <katieschleper@gmail.com>
2024-05-08 11:00:37 -0400

    Modified the Py program

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-name = input()
+firstName = input('Please enter your first name into the console. ')
+lastName = input('Please enter your last name into the console. ')
 
-print(f'Hello there, {name}!')
+print(f'Hello there, {firstName} {lastName}!')
