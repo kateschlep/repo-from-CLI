8668550351ccaad99e6904e832020ae034fb5116
Kate <144851274+kateschlep@users.noreply.github.com>
2024-05-08 12:26:31 -0400

    Testing commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-firstName = input('Please enter your first name into the console. ')
-lastName = input('Please enter your last name into the console.')
+firstName = input('Please enter your first name into the console: ')
+lastName = input('Please enter your last name into the console: ')
 
-print(f'Hi {firstName} {lastName}xxx!')
+print(f'Hi {firstName} {lastName}!')
